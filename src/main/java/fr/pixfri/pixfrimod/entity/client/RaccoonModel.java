309ca598871ca3d7067813d171ba17558c538456
Pixfri <Jean.letessier@protonmail.com>
2022-06-07 21:57:38 +0200

    Raccoon has different variants, now it's looking at you

--- src/main/java/fr/pixfri/pixfrimod/entity/client/RaccoonModel.java
+++ src/main/java/fr/pixfri/pixfrimod/entity/client/RaccoonModel.java
@@ -3,7 +3,10 @@ package fr.pixfri.pixfrimod.entity.client;
 import fr.pixfri.pixfrimod.PixfriMod;
 import fr.pixfri.pixfrimod.entity.custom.RaccoonEntity;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class RaccoonModel extends AnimatedGeoModel<RaccoonEntity> {
     @Override
@@ -13,11 +16,24 @@ public class RaccoonModel extends AnimatedGeoModel<RaccoonEntity> {
 
     @Override
     public ResourceLocation getTextureLocation(RaccoonEntity object) {
-        return new ResourceLocation(PixfriMod.MOD_ID, "textures/entity/raccoon.png");
+        return RaccoonRenderer.LOCATION_BY_VARIANT.get(object.getVariant());
     }
 
     @Override
     public ResourceLocation getAnimationFileLocation(RaccoonEntity animatable) {
         return new ResourceLocation(PixfriMod.MOD_ID, "animations/raccoon.animation.json");
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Override
+    public void setLivingAnimations(RaccoonEntity entity, Integer uniqueID, AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+        IBone head = this.getAnimationProcessor().getBone("head");
+
+        EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+        if(head != null) {
+            head.setRotationX(extraData.headPitch * ((float) Math.PI / 180F));
+            head.setRotationY(extraData.netHeadYaw * ((float) Math.PI / 180F));
+        }
     }
 }
--- src/main/java/fr/pixfri/pixfrimod/entity/custom/RaccoonEntity.java
+++ src/main/java/fr/pixfri/pixfrimod/entity/custom/RaccoonEntity.java
@@ -1,7 +1,9 @@
 package fr.pixfri.pixfrimod.entity.custom;
 
 import fr.pixfri.pixfrimod.entity.ModEntityTypes;
+import fr.pixfri.pixfrimod.entity.variant.RaccoonVariant;
 import fr.pixfri.pixfrimod.item.ModItems;
+import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
@@ -10,12 +12,11 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
@@ -25,6 +26,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.scores.Team;
 import net.minecraftforge.event.ForgeEventFactory;
@@ -42,6 +44,9 @@ public class RaccoonEntity extends TamableAnimal implements IAnimatable {
 
     private static final EntityDataAccessor<Boolean> SITTING =
             SynchedEntityData.defineId(RaccoonEntity.class, EntityDataSerializers.BOOLEAN);
+
+    private static final EntityDataAccessor<Integer> DATA_ID_TYPE_VARIANT =
+            SynchedEntityData.defineId(RaccoonEntity.class, EntityDataSerializers.INT);
 
     public RaccoonEntity(EntityType<? extends TamableAnimal> entityType, Level level) {
         super(entityType, level);
@@ -71,7 +76,10 @@ public class RaccoonEntity extends TamableAnimal implements IAnimatable {
     @Nullable
     @Override
     public AgeableMob getBreedOffspring(ServerLevel serverLevel, AgeableMob mob) {
-        return ModEntityTypes.RACCOON.get().create(serverLevel);
+        RaccoonEntity baby = ModEntityTypes.RACCOON.get().create(serverLevel);
+        RaccoonVariant variant = Util.getRandom(RaccoonVariant.values(), this.random);
+        baby.setVariant(variant);
+        return baby;
     }
 
     @Override
@@ -175,18 +183,21 @@ public class RaccoonEntity extends TamableAnimal implements IAnimatable {
     public void readAdditionalSaveData(CompoundTag tag) {
         super.readAdditionalSaveData(tag);
         setSitting(tag.getBoolean("isSitting"));
+        this.entityData.set(DATA_ID_TYPE_VARIANT, tag.getInt("Variant"));
     }
 
     @Override
     public void addAdditionalSaveData(CompoundTag tag) {
         super.addAdditionalSaveData(tag);
         tag.putBoolean("isSitting", this.isSitting());
+        tag.putInt("Variant", this.getTypeVariant());
     }
 
     @Override
     protected void defineSynchedData() {
         super.defineSynchedData();
         this.entityData.define(SITTING, false);
+        this.entityData.define(DATA_ID_TYPE_VARIANT, 0);
     }
 
     public void setSitting(boolean sitting) {
@@ -220,5 +231,27 @@ public class RaccoonEntity extends TamableAnimal implements IAnimatable {
             getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(2D);
             getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue((double)0.25f);
         }
+    }
+
+    /* VARIANTS */
+    @Override
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor p_146746_, DifficultyInstance p_146747_,
+                                        MobSpawnType p_146748_, @Nullable SpawnGroupData p_146749_,
+                                        @Nullable CompoundTag p_146750_) {
+        RaccoonVariant variant = Util.getRandom(RaccoonVariant.values(), this.random);
+        setVariant(variant);
+        return super.finalizeSpawn(p_146746_, p_146747_, p_146748_, p_146749_, p_146750_);
+    }
+
+    public RaccoonVariant getVariant() {
+        return RaccoonVariant.byId(this.getTypeVariant() & 255);
+    }
+
+    private int getTypeVariant() {
+        return this.entityData.get(DATA_ID_TYPE_VARIANT);
+    }
+
+    private void setVariant(RaccoonVariant variant) {
+        this.entityData.set(DATA_ID_TYPE_VARIANT, variant.getId() & 255);
     }
 }
